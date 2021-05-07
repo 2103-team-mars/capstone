15762beecf377e323a7d101552dc85a7455dd166
Mohammad Mussab <Mmussab39@gmail.com>
2021-05-07 16:49:59 -0400

    added infoWindow to to markers

--- client/components/MapComponent.js
+++ client/components/MapComponent.js
@@ -5,6 +5,40 @@ import { fetchDoctors } from '../store/googleMap';
 import { connect } from 'react-redux';
 
 export class MapComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activeMarker: {},
+      selectedDoctor: {},
+      showingInfoWindow: false,
+    };
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onInfoWindowClose = this.onInfoWindowClose.bind(this);
+    this.onMapClicked = this.onMapClicked.bind(this);
+  }
+
+  onMarkerClick(props, marker) {
+    this.setState({
+      activeMarker: marker,
+      selectedDoctor: props,
+      showingInfoWindow: true,
+    });
+  }
+
+  onInfoWindowClose() {
+    this.setState({
+      activeMarker: null,
+      showingInfoWindow: false,
+    });
+  }
+
+  onMapClicked() {
+    if (this.state.showingInfoWindow)
+      this.setState({
+        activeMarker: null,
+        showingInfoWindow: false,
+      });
+  }
   componentDidMount() {
     this.props.fetchDoctors();
   }
@@ -27,7 +61,10 @@ export class MapComponent extends Component {
           {doctors.map((doctor) => {
             return (
               <Marker
+                onClick={this.onMarkerClick}
                 name={doctor.name}
+                email={doctor.email}
+                location={doctor.location}
                 position={{
                   lat: doctor.coordinates[0],
                   lng: doctor.coordinates[1],
@@ -35,6 +72,18 @@ export class MapComponent extends Component {
               />
             );
           })}
+          <InfoWindow
+            marker={this.state.activeMarker}
+            onClose={this.onInfoWindowClose}
+            visible={this.state.showingInfoWindow}
+          >
+            <div>
+              <h1>{this.state.selectedDoctor.name}</h1>
+              <h1>{this.state.selectedDoctor.location}</h1>
+              <h1>{this.state.selectedDoctor.email}</h1>
+              <h1>{this.state.selectedDoctor.id}</h1>
+            </div>
+          </InfoWindow>
         </Map>
       </div>
     );
