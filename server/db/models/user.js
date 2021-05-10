0cbada8535758d11265d10c1d2b132d717c0d2b1
Sarahjean Marhone <sjmarhone@gmail.com>
2021-05-10 18:01:35 -0400

    BE refactor: DB models to polymorphic associations

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,12 +1,12 @@
-const Sequelize = require('sequelize');
-const db = require('../db');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const makeCoordinates = require('../../coordinates');
+const Sequelize = require("sequelize");
+const db = require("../db");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const makeCoordinates = require("../../coordinates");
 
 const SALT_ROUNDS = 5;
 
-const User = db.define('user', {
+const User = db.define("user", {
   email: {
     type: Sequelize.STRING,
     unique: true,
@@ -15,7 +15,11 @@ const User = db.define('user', {
       isEmail: true,
     },
   },
-  name: {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  lastName: {
     type: Sequelize.STRING,
     allowNull: false,
   },
@@ -23,15 +27,10 @@ const User = db.define('user', {
     type: Sequelize.STRING,
     allowNull: false,
   },
-  isDoctor: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-    defaultValue: false,
-  },
   profilePicture: {
     type: Sequelize.TEXT,
     defaultValue:
-      'https://rpgplanner.com/wp-content/uploads/2020/06/no-photo-available.png',
+      "https://rpgplanner.com/wp-content/uploads/2020/06/no-photo-available.png",
   },
   age: {
     type: Sequelize.INTEGER,
@@ -40,7 +39,7 @@ const User = db.define('user', {
     },
   },
   sex: {
-    type: Sequelize.ENUM('male', 'female'),
+    type: Sequelize.ENUM("male", "female"),
     allowNull: false,
   },
   dob: {
@@ -57,6 +56,13 @@ const User = db.define('user', {
   coordinates: {
     type: Sequelize.ARRAY(Sequelize.FLOAT),
   },
+  metaType: {
+    type: Sequelize.ENUM("doctor", "patient"),
+    allowNull: false,
+  },
+  metaId: {
+    type: Sequelize.INTEGER,
+  },
 });
 
 module.exports = User;
@@ -64,6 +70,26 @@ module.exports = User;
 /**
  * instanceMethods
  */
+
+User.addHook("afterFind", (findResult) => {
+  if (!Array.isArray(findResult)) findResult = [findResult];
+  for (const instance of findResult) {
+    if (instance.metaType === "doctor" && instance.doctor !== undefined) {
+      instance.meta = instance.doctor;
+    } else if (
+      instance.metaType === "patient" &&
+      instance.patient !== undefined
+    ) {
+      instance.meta = instance.patient;
+    }
+    // To prevent mistakes:
+    delete instance.doctor;
+    delete instance.dataValues.doctor;
+    delete instance.patient;
+    delete instance.dataValues.patient;
+  }
+});
+
 User.prototype.correctPassword = function (candidatePwd) {
   //we need to compare the plain version to an encrypted version of the password
   return bcrypt.compare(candidatePwd, this.password);
@@ -79,7 +105,7 @@ User.prototype.generateToken = function () {
 User.authenticate = async function ({ email, password }) {
   const user = await this.findOne({ where: { email } });
   if (!user || !(await user.correctPassword(password))) {
-    const error = Error('Incorrect email/password');
+    const error = Error("Incorrect email/password");
     error.status = 401;
     throw error;
   }
@@ -91,11 +117,11 @@ User.findByToken = async function (token) {
     const { id } = await jwt.verify(token, process.env.JWT);
     const user = User.findByPk(id);
     if (!user) {
-      throw 'nooo';
+      throw "nooo";
     }
     return user;
   } catch (ex) {
-    const error = Error('bad token');
+    const error = Error("bad token");
     error.status = 401;
     throw error;
   }
@@ -108,14 +134,14 @@ const hashPassword = async (user) => {
   //in case the password has been changed, we want to encrypt it with bcrypt
   // console.log(user.changed());
   // console.log(user.changed('password'));
-  if (user.changed('password')) {
+  if (user.changed("password")) {
     user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
   }
 };
 
 const convertAddress = async (user) => {
-  if (user.changed('location')) {
-    console.log('in convertAddress');
+  if (user.changed("location")) {
+    console.log("in convertAddress");
     user.coordinates = await makeCoordinates(user.location);
   }
 };
