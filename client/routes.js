98021a4f49a1deaed92d47c6ad898f25da2c09a7
Chris Gil Martins <cgilmartins@gmail.com>
2021-05-10 15:57:46 -0400

    heroku casing fix

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { Login, Signup } from './components/AuthForm';
-import Home from './components/Home';
+import Home from './components/home';
 import { me } from './store';
 import MapComponent from './components/MapComponent';
 import Meeting from './components/Meeting';
@@ -22,19 +22,19 @@ class Routes extends Component {
       <div>
         {isLoggedIn ? (
           <Switch>
-            <Route path='/home' component={Home} />
+            <Route path="/home" component={Home} />
 
-            <Route path='/map' component={MapComponent} />
-            <Route path='/meeting' component={Meeting} />
-            <Redirect to='/home' />
+            <Route path="/map" component={MapComponent} />
+            <Route path="/meeting" component={Meeting} />
+            <Redirect to="/home" />
           </Switch>
         ) : (
           <Switch>
-            <Route path='/' exact component={Login} />
-            <Route path='/login' component={Login} />
-            <Route path='/signup' component={Signup} />
-            <Route path='/meeting' component={Meeting} />
-            <Route path='/map' component={MapComponent} />
+            <Route path="/" exact component={Login} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/meeting" component={Meeting} />
+            <Route path="/map" component={MapComponent} />
           </Switch>
         )}
       </div>
