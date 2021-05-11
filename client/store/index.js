80cd16c954e50289b7c8b9295e0d299f24bd3811
Sarahjean Marhone <sjmarhone@gmail.com>
2021-05-11 16:47:51 -0400

    FE: builds a single doctor redux reducer and thunk

--- client/store/index.js
+++ client/store/index.js
@@ -1,13 +1,15 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { createLogger } from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import auth from './auth';
-import mapReducer from './googleMap';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import auth from "./auth";
+import mapReducer from "./googleMap";
+import singleDoctor from "./singleDoctor";
 
 const reducer = combineReducers({
   auth,
   mapDoctors: mapReducer,
+  singleDoctor,
 });
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
@@ -15,4 +17,4 @@ const middleware = composeWithDevTools(
 const store = createStore(reducer, middleware);
 
 export default store;
-export * from './auth';
+export * from "./auth";
