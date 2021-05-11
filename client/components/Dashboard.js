6dd21643ee361c4762efdde4a10ccd3d51e1c666
Mohammad Mussab <Mmussab39@gmail.com>
2021-05-11 17:10:59 -0400

    feature: added tabs to dashboard

---
+++ client/components/Dashboard.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import { AppBar, Tabs, Tab, Typography, Box } from '@material-ui/core';
+import { connect } from 'react-redux';
+import MapComponent from './MapComponent';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role='tabpanel'
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+export function Dashboard({ auth }) {
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <div>
+      {auth.metaType === 'patient' ? (
+        <div>
+          <AppBar position='static'>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor='primary'
+              textColor='primary'
+              centered
+              variant='fullWidth'
+            >
+              <Tab label='Find Doctor' />
+              <Tab label='My Doctors' />
+              <Tab label='Profile' />
+              <Tab label='Medications' />
+              <Tab label='Appointments' />
+            </Tabs>
+          </AppBar>
+          <TabPanel value={value} index={0}>
+            <MapComponent />
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            My doctors list
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            My Profile
+          </TabPanel>
+          <TabPanel value={value} index={3}>
+            My Medications
+          </TabPanel>
+          <TabPanel value={value} index={4}>
+            My Appointments
+          </TabPanel>
+        </div>
+      ) : (
+        <div>
+          <AppBar position='static'>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor='primary'
+              textColor='primary'
+              centered
+              variant='fullWidth'
+            >
+              <Tab label='Profile' />
+              <Tab label='My Patients' />
+              <Tab label='My Appointments' />
+            </Tabs>
+          </AppBar>
+          <TabPanel value={value} index={0}>
+            My Doctor Profile
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            My Patients
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            My Appointments as a doctor
+          </TabPanel>
+        </div>
+      )}
+    </div>
+  );
+}
+const mapState = (state) => {
+  return {
+    auth: state.auth,
+  };
+};
+export default connect(mapState)(Dashboard);
