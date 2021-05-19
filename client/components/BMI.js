aeb6d9dbf3e66a5b3d378c8784c649586d105014
Chiara Wahsono <chiarawahsono@gmail.com>
2021-05-19 16:46:19 -0400

    added ImperialBMI and MetricBMI components

--- client/components/BMI.js
+++ client/components/BMI.js
@@ -1,49 +1,37 @@
 import React, { useEffect, useState } from "react";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Switch from "@material-ui/core/Switch";
+import MetricBMI from "./MetricBMI";
+import ImperialBMI from "./ImperialBMI";
 
 function BMI() {
   const [state, setState] = React.useState({
     checkedA: true,
-    checkedB: true,
   });
-  const [height, setHeight] = useState(0);
-  const [weight, setWeight] = useState(0);
-  const [bmi, setBMI] = useState(0);
-
-  const handleHeight = (event) => {
-    let height = event.target.value;
-    setHeight(height);
-  };
-
-  const handleWeight = (event) => {
-    let weight = event.target.value;
-    setWeight(weight);
-  };
 
   const handleChange = (event) => {
     setState({ ...state, [event.target.name]: event.target.checked });
   };
 
-  useEffect(() => {
-    let bmiMetric = (weight / (height * 2)) * 100;
-    setBMI(bmiMetric.toFixed(2));
-  }, [height, weight]);
-
   return (
     <div>
       <div className="BMI">
         <h1>BMI CALCULATOR</h1>
-        <Switch
-          checked={state.checkedA}
-          onChange={handleChange}
-          name="checkedA"
-          inputProps={{ "aria-label": "secondary checkbox" }}
-        />
-        <p>Height</p>
-        <input type="number" onChange={handleHeight}></input>
-        <p>Weight</p>
-        <input type="number" onChange={handleWeight}></input>
-        <h2>BMI: {bmi}</h2>
+        <FormGroup row>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={state.checkedA}
+                onChange={handleChange}
+                name="checkedA"
+              />
+            }
+            label={state.checkedA ? "Metric" : "Imperial"}
+          />
+        </FormGroup>
+        {state.checkedA ? <MetricBMI /> : <ImperialBMI />}
+
         <img src="https://westlakeplasticsurgery.com/wp-content/uploads/2020/02/BMI-Chart-Detailed.png" />
       </div>
     </div>
---
+++ client/components/ImperialBMI.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+const ImperialBMI = () => {
+  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [bmi, setBMI] = useState(0);
+
+  const handleHeight = (event) => {
+    let height = event.target.value;
+    setHeight(height);
+  };
+
+  const handleWeight = (event) => {
+    let weight = event.target.value;
+    setWeight(weight);
+  };
+  useEffect(() => {
+    let bmiImperial = (weight / (height ^ 2)) * 703;
+    setBMI(bmiImperial.toFixed(2));
+  }, [height, weight]);
+
+  return (
+    <div>
+      <p>Height in inches</p>
+      <input type="number" onChange={handleHeight}></input>
+      <p>Weight in lbs</p>
+      <input type="number" onChange={handleWeight}></input>
+      <h2>BMI: {bmi}</h2>
+    </div>
+  );
+};
+export default ImperialBMI;
