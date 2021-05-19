033b45ad8864ffc04a8f8865149cff6930989fd9
Chiara Wahsono <chiarawahsono@gmail.com>
2021-05-19 15:27:13 -0400

    added toggle button

--- client/components/BMI.js
+++ client/components/BMI.js
@@ -1,40 +1,51 @@
 import React, { useEffect, useState } from "react";
-import ".components/";
+import Switch from "@material-ui/core/Switch";
 
 function BMI() {
+  const [state, setState] = React.useState({
+    checkedA: true,
+    checkedB: true,
+  });
   const [height, setHeight] = useState(0);
   const [weight, setWeight] = useState(0);
   const [bmi, setBMI] = useState(0);
 
-  function handleHeight(event) {
+  const handleHeight = (event) => {
     let height = event.target.value;
     setHeight(height);
-  }
+  };
 
-  function handleWeight(event) {
+  const handleWeight = (event) => {
     let weight = event.target.value;
     setWeight(weight);
-  }
+  };
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
 
   useEffect(() => {
-    if (isToggled) {
-      let bmiMetric = (weight / (height * 2)) * 100;
-      setBMI(bmiMetric.toFixed(2));
-    } else {
-      let bmiImperial = (weight / (height * 2)) * 100;
-      setBMI(bmiImperial.toFixed(2));
-    }
+    let bmiMetric = (weight / (height * 2)) * 100;
+    setBMI(bmiMetric.toFixed(2));
   }, [height, weight]);
 
   return (
-    <div className="BMI">
-      <h1>BMI CALCULATOR</h1>
-      <p>Height</p>
-      <input type="number" onChange={handleHeight}></input>
-      <p>Weight</p>
-      <input type="number" onChange={handleWeight}></input>
-      <h2>BMI: {bmi}</h2>
-      <img src="https://westlakeplasticsurgery.com/wp-content/uploads/2020/02/BMI-Chart-Detailed.png" />
+    <div>
+      <div className="BMI">
+        <h1>BMI CALCULATOR</h1>
+        <Switch
+          checked={state.checkedA}
+          onChange={handleChange}
+          name="checkedA"
+          inputProps={{ "aria-label": "secondary checkbox" }}
+        />
+        <p>Height</p>
+        <input type="number" onChange={handleHeight}></input>
+        <p>Weight</p>
+        <input type="number" onChange={handleWeight}></input>
+        <h2>BMI: {bmi}</h2>
+        <img src="https://westlakeplasticsurgery.com/wp-content/uploads/2020/02/BMI-Chart-Detailed.png" />
+      </div>
     </div>
   );
 }
