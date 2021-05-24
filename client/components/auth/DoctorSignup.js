ea23471d7738a0953428fc65a3cfa87ba8fd621d
Chris Gil Martins <cgilmartins@gmail.com>
2021-05-24 14:46:44 -0400

    fix doctor registration

--- client/components/auth/DoctorSignup.js
+++ client/components/auth/DoctorSignup.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
-const DoctorSignup = () => {
-  return <div></div>;
+import { Box, TextField, MenuItem } from '@material-ui/core';
+
+const professions = ['Psychiatrist', 'Psychologist', 'Therapist'];
+
+const DoctorSignup = ({ professionRef }) => {
+  return (
+    <>
+      <Box width="100%" pt={2}>
+        <TextField
+          inputRef={professionRef}
+          id="profession"
+          name="profession"
+          label="Profession"
+          variant="outlined"
+          defaultValue="Psychiatrist"
+          fullWidth
+          required
+          select
+        >
+          {professions.map((option, idx) => (
+            <MenuItem key={idx} value={option}>
+              {option}
+            </MenuItem>
+          ))}
+        </TextField>
+      </Box>
+    </>
+  );
 };
 
 export default DoctorSignup;
