27029e77342b8dfdd6d19b3ca390d68713c75d35
Sarahjean Marhone <sjmarhone@gmail.com>
2021-05-24 14:01:01 -0400

    FE: updates logo with SVG

--- client/components/AboutUs.js
+++ client/components/AboutUs.js
@@ -1,40 +1,40 @@
-import React, { Component } from 'react';
-import { Box, Grid, Typography } from '@material-ui/core';
-import About from '../SVG/AboutUs';
+import React, { Component } from "react";
+import { Box, Grid, Typography, Paper } from "@material-ui/core";
+import About from "../SVG/AboutUs";
 
 const styles = {
   gridContainer: {
-    display: 'grid',
-    padding: '1rem',
-    justify: 'space-evenly',
-    alignItems: 'center',
-    gridGap: '1rem',
+    display: "grid",
+    padding: "1rem",
+    justify: "space-evenly",
+    alignItems: "center",
+    gridGap: "1rem",
   },
   gridAbout: {
-    padding: '1rem',
-    borderRadius: '1rem',
-    boxShadow: '0 0 0.5rem 0 rgb(0 0 0 / 25%)',
-    gridArea: 'Our Mission',
-    backgroundColor: '#f5f5f5',
+    padding: "1rem",
+    borderRadius: "1rem",
+    boxShadow: "0 0 0.5rem 0 rgb(0 0 0 / 25%)",
+    gridArea: "Our Mission",
+    backgroundColor: "#f5f5f5",
     spacing: 3,
   },
   title: {
     fontSize: 28,
-    color: '#f5f5f5',
-    alignItems: 'center',
-    backgroundColor: '#6BAF5A',
-    padding: '1px',
-    borderRadius: '1rem',
+    color: "#f5f5f5",
+    alignItems: "center",
+    backgroundColor: "#6BAF5A",
+    padding: "1px",
+    borderRadius: "1rem",
   },
   text: {
-    alignItems: 'center',
-    padding: '15px',
+    alignItems: "center",
+    padding: "15px",
   },
   img: {
     width: 256,
-    height: 'auto',
-    margin: '0 auto',
-    display: 'block',
+    height: "auto",
+    margin: "0 auto",
+    display: "block",
   },
 };
 
@@ -42,15 +42,21 @@ export class AboutUs extends Component {
   render() {
     return (
       <Box>
-        <Grid container container spacing={3} direction="column" style={styles.gridContainer}>
+        <Grid
+          container
+          container
+          spacing={3}
+          direction="column"
+          style={styles.gridContainer}
+        >
           <About />
           <Grid item xs={12} style={styles.gridAbout}>
             <Typography variant="h6" align="center" style={styles.title}>
               Our Mission
             </Typography>
-            <Typography style={styles.text}>
-              Making selecting and managing your mental health easier and more accessible. Because
-              finding and prioritizing your mental health professional should be easy and convenient
+            <Typography align="center" style={styles.text}>
+              Making selecting and managing your mental health easy, seamless
+              and at your fingertips.
             </Typography>
           </Grid>
 
@@ -58,10 +64,10 @@ export class AboutUs extends Component {
             <Typography variant="h6" align="center" style={styles.title}>
               Our Providers
             </Typography>
-            <Typography style={styles.text}>
-              Hello Health offers access to licensed, trained, experienced, and accredited
-              psychiatrist, psychologists (PhD / PsyD), and board licensed professional counselors
-              and therapists (LPC).
+            <Typography align="center" style={styles.text}>
+              Hello Health offers access to licensed, trained, experienced, and
+              accredited psychiatrist, psychologists, and board licensed
+              professional counselors and therapists.
             </Typography>
           </Grid>
 
@@ -69,10 +75,11 @@ export class AboutUs extends Component {
             <Typography variant="h6" align="center" style={styles.title}>
               Our Team
             </Typography>
-            <Typography style={styles.text}>
-              We are passionate and compassionate professionals team of developers who are driven by
-              our mission. In developing this application we hope to build a resource for mental
-              health. Check out our team!
+            <Typography align="center" style={styles.text}>
+              We are a passionate, professional team of developers who are
+              driven by our mission and devoted to our craft. In developing this
+              application we hope to build a resource for better mental health
+              management. Check out our team!
             </Typography>
             <Grid container spacing={1} justify="center" alignItems="center">
               <Grid item xs>
@@ -81,6 +88,9 @@ export class AboutUs extends Component {
                   alt="headshot"
                   src="https://ca.slack-edge.com/T024FPYBQ-U01JGF243E0-89e19e4353a9-512"
                 />
+                <p justify="center" align="center">
+                  Chiara Wahsono
+                </p>
               </Grid>
               <Grid item xs>
                 <img
@@ -88,6 +98,9 @@ export class AboutUs extends Component {
                   alt="headshot"
                   src="https://ca.slack-edge.com/T024FPYBQ-U01PXDH1PTK-65bf8620e716-512"
                 />
+                <p justify="center" align="center">
+                  Christopher Gil Martins
+                </p>
               </Grid>
 
               <Grid item xs>
@@ -96,6 +109,9 @@ export class AboutUs extends Component {
                   alt="headshot"
                   src="https://ca.slack-edge.com/T024FPYBQ-U01HVGXDKL7-9f1006440219-512"
                 />
+                <p justify="center" align="center">
+                  Mohammad Mussab
+                </p>
               </Grid>
 
               <Grid item xs>
@@ -104,6 +120,9 @@ export class AboutUs extends Component {
                   alt="headshot"
                   src="https://ca.slack-edge.com/T024FPYBQ-U01PFMW4F0V-366ee5aa3832-512"
                 />
+                <p justify="center" align="center">
+                  Sarahjean Marhône
+                </p>
               </Grid>
             </Grid>
           </Grid>
